a6ee6649e9cbc3ccf8e474dd084a13441e901fc8
yamatomo73 <>
2019-06-04 23:08:35 +0900

    クライアントAPI試作

--- src/JiraApiClient.ts
+++ src/JiraApiClient.ts
@@ -14,8 +14,63 @@ class JiraApiClient {
         this.api_token = api_token;
     }
 
+    private createApiUrl(resource_name: string): string {
+        return Utilities.formatString('https://%s.atlassian.net/%s/%s', this.hostname, JiraApiClient.BASE_API_PATH, resource_name);
+    }
+
+    /**
+     * 全てのプロジェクトを取得する
+     * @link https://developer.atlassian.com/cloud/jira/platform/rest/v3/?utm_source=%2Fcloud%2Fjira%2Fplatform%2Frest&utm_medium=302#api-rest-api-3-project-get
+     */
+    public getAllProjects(): Object {
+        const url: string = this.createApiUrl('project');
+        return this.requestGitHubApi(url, 'get');
+    }
+
+    /**
+     * ユーザーが1つ以上のプロジェクトに対するプロジェクトの参照プロジェクト権限を持っている場合、ユーザーが参照する権限を持つプロジェクトに関連付けられている課題タイプが返されます。
+     * @link https://developer.atlassian.com/cloud/jira/platform/rest/v3/?utm_source=%2Fcloud%2Fjira%2Fplatform%2Frest&utm_medium=302#api-rest-api-3-issuetype-get
+     */
+    public getAllIssueTypesForUser(): Object {
+        const url: string = this.createApiUrl('issuetype');
+        return this.requestGitHubApi(url, 'get');
+    }
+
+    /**
+     * チケットを参照する
+     * @link https://developer.atlassian.com/cloud/jira/platform/rest/v3/?utm_source=%2Fcloud%2Fjira%2Fplatform%2Frest&utm_medium=302#api-rest-api-3-issue-issueIdOrKey-get
+     * 
+     * @param issueIdOrKey 
+     * @param fields 
+     */
     public getIssueByissueIdOrKey(issueIdOrKey: string, fields?: Array<string>): Object {
-        return this.requestGitHubApi(Utilities.formatString('https://%s.atlassian.net/%s/issue/%s', this.hostname, JiraApiClient.BASE_API_PATH, issueIdOrKey), 'get');
+        const url: string = this.createApiUrl(Utilities.formatString('issue/%s', issueIdOrKey));
+        return this.requestGitHubApi(url, 'get');
+    }
+
+    /**
+     * チケットを作成する
+     * @link https://developer.atlassian.com/cloud/jira/platform/rest/v3/?utm_source=%2Fcloud%2Fjira%2Fplatform%2Frest&utm_medium=302#api-rest-api-3-issue-post
+     *
+     * @param project_id 
+     * @param issueType 
+     * @param reporter 
+     */
+    public createIssue(projectId: string, issueType: string, priority: string, reporter: string, summary: string, labels: Array<string>): Object {
+        const url: string = this.createApiUrl('issue');
+        return this.requestGitHubApi(url, 'post',
+            {
+                'update': {},
+                'fields': {
+                    'project': projectId,
+                    'issuetype': issueType,
+                    'priority': priority,
+                    'reporter': reporter,
+                    'summary': summary,
+                    'description': 'チケット説明',
+                },
+            }
+        );
     }
 
     /**
@@ -24,6 +79,7 @@ class JiraApiClient {
     private requestGitHubApi(url: string, method: 'get' | 'delete' | 'patch' | 'post' | 'put', payload?: Object): Object {
         const header: Object = {
             'Content-Type': 'application/json',
+            'Accept': 'application/json',
             'Authorization': Utilities.formatString('Basic %s', Utilities.base64Encode(this.email + ":" + this.api_token)),
         };
         const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
@@ -39,4 +95,10 @@ class JiraApiClient {
         var response_body = response.getContentText();
         return JSON.parse(response_body);
     }
+}
+
+class CreateIssueRequest {
+    constructor() {
+    }
+
 }
